de847a7b78fb6f0ca74fa76c25f9eb488fcff7d7
lazytime <0205miss@gmail.com>
2024-06-24 01:16:38 +0800

    add earth globe on page

--- src/components/ui/bentogrid.tsx
+++ src/components/ui/bentogrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/utils/cn";
+import { GlobeDisplay } from "./globedisplay";
 
 export const BentoGrid = ({
   className,
@@ -41,20 +42,23 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "bg-card dark:bg-card row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:border-white/[0.2] border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
-      style={{
-        background: "rgb(4,7,29)",
-      }}
     >
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+      <div
+        className={cn(
+          titleClassName,
+          "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+        )}
+      >
+        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200">
           {title}
         </div>
         <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
           {description}
         </div>
+        {id === 2 && <GlobeDisplay />}
       </div>
     </div>
   );
